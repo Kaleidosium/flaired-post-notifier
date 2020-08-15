bef15e7d6d403a54f99160dcbda180a5aef7d398
iamrifki <iamrifki0@gmail.com>
2020-08-15 10:10:10 +0700

    Publish to GitHub

---
+++ README.md
@@ -0,0 +1,3 @@
+# flaired-post-notifier
+A script which sends you a notification via notify.run when a new post with a certain flair gets posted on a subreddit.
+
---
+++ main.py
@@ -0,0 +1,31 @@
+from notify_run import Notify
+import config
+import praw
+
+reddit = praw.Reddit(
+    client_id=config.client_id,
+    client_secret=config.client_secret,
+    user_agent=config.user_agent,
+    username=config.username,
+    password=config.password,
+)
+
+
+def send_link_notification(link):
+    notify.send("New Flaired Post!", link)
+
+
+def get_post_with_flair_from_subreddit(subreddit, flair):
+    _subreddit = reddit.subreddit(subreddit)
+
+    for submission in _subreddit.stream.submissions(skip_existing=True):
+        _flair = submission.link_flair_text
+
+        if _flair == flair:
+            send_link_notification(submission.url)
+
+if __name__ == "__main__":
+    notify = Notify(endpoint=config.endpoint)
+
+    get_post_with_flair_from_subreddit("ICanDrawThat", "Offer")
+    get_post_with_flair_from_subreddit("DrawForMe", "Free Offer")
